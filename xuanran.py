8009a400807094fec6a9a0caff9f5edd32bf4582
ljxxcaijing <755763949@qq.com>
2016-04-26 23:18:07 +0800

    Delete xuanran.py

--- xuanran.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-        return render_template('hello.html', name=name)
-   
-   '''render_template方法可以渲染模版'''
