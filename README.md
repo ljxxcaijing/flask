55f8ce2efd6cfdb40a3865bee0aeb3c1b364d792
ljxxcaijing <755763949@qq.com>
2016-05-02 23:19:30 +0800

    “commit”

---
+++ README.md
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+Flasky
+======
+
+This repository contains the source code examples for my O'Reilly book [Flask Web Development](http://www.flaskbook.com).
+
+The commits and tags in this repository were carefully created to match the sequence in which concepts are presented in the book. Please read the section titled "How to Work with the Example Code" in the book's preface for instructions.
+
+=======
+# flask
+###flask exercise
+
+ MY FLASK exercise
+>>>>>>> 5303fd70beed81ea151c384c9a83d62474f761d1
---
+++ hello.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+from flask import Flask, render_template
+from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+class Nameform(Form):
+	name = StringField('What is your name?', validators=[Required()])
+	submit = SubmitField('Submit')
+	
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           current_time=datetime.utcnow())
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+if __name__ == '__main__':
+    manager.run()
+
