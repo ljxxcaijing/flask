4ed6cd11e3f9c8519bc8685d56eadb16d488be12
ljxxcaijing <755763949@qq.com>
2016-05-09 21:56:12 +0800

    “5.9commit”

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
@@ -7,7 +7,7 @@ from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
-class Nameform(Form):
+class NameForm(Form):
 	name = StringField('What is your name?', validators=[Required()])
 	submit = SubmitField('Submit')
 	
@@ -19,6 +19,8 @@ manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -30,17 +32,19 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',
-                           current_time=datetime.utcnow())
-
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
 
-
 if __name__ == '__main__':
+    app.debug = True
     manager.run()
 
